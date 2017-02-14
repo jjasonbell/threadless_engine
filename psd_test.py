9e6a9183c9939358318c6cdf0c7e35b9d405a731
jjasonbell <j.jason.bell@gmail.com>
2017-02-14 09:28:35 -0600

    Add color_swap function

--- psd_test.py
+++ psd_test.py
@@ -1,14 +1,23 @@
 import os, subprocess
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+import numpy as np
 from psd_tools import PSDImage
 from PIL import Image
-
 
 
 def img_resize(target_width, img):
     w_pct = (target_width / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(w_pct)))
     return img.resize((target_width, hsize), Image.ANTIALIAS)
+
+def color_swap(from_triple, to_triple, img):
+    data = np.array(img)
+    r1, g1, b1 = from_triple
+    r2, g2, b2 = to_triple
+    red, green, blue = data[:,:,0], data[:,:,1], data[:,:,2]
+    mask = (red == r1) & (green == g1) & (blue == b1)
+    data[:,:,:3][mask] = [r2, g2, b2]
+    return Image.fromarray(data)
 
 psd = PSDImage.load('tee_template.psd')
 background = Image.new('RGBA', (1200, 1200), 'white')
